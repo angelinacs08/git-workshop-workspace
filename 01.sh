5f7fafb6381a7afe9147baf0b2394ada20186a85
angelina.siegrist@anu.edu.au <angelina.siegrist_anu@localhost>
2020-06-17 10:56:51 +1000

    git add 01.sh

---
+++ 01.sh
@@ -0,0 +1,4 @@
+echo "this is my data-download script"
+# it downloads some data
+mkdir data/
+wget -O data/genes.bed  https://github.com/JanaSperschneider/Python_Intro_ANU_RSB/raw/master/datasets/IAA_up.gene.bed6
